f34c476fed47e721523dde1cee1aab58f82fe5cc
Joban Arora <jobanarora@Jobans-MacBook-Air.local>
2023-08-06 01:15:38 -0400

    chalpa

--- lib/actions.ts
+++ lib/actions.ts
@@ -13,18 +13,11 @@ import { json } from "stream/consumers";
 import { FormState, ProjectForm } from "@/common.types";
 import { categoryFilters } from "@/constant";
 
-const isProduction = process.env.NODE_ENV === "production";
-const apiUrl = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
-  : "http://127.0.0.1:4000/graphql";
+const isProduction = process.env.NODE_ENV === 'production';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
-const apiKey = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
-  : "icandoit";
-
-const serverUrl = isProduction
-  ? process.env.NEXT_PUBLIC_SERVER_URL
-  : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
